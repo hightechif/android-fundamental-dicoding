fff0dafbaa149dfe75cfe4ad2752507d5505bbf3
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-08-06 16:24:41 +0700

    Feat(Fundamental): My App Bar

---
+++ MyAppBar/app/src/main/java/dicoding/fadhil/myappbar/MenuActivity.kt
@@ -0,0 +1,11 @@
+package dicoding.fadhil.myappbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
